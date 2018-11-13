281851e6eb6a72d47bbc19659b1edfa3d050297b
Maciej Koziara <kozpacific@gmail.com>
2018-11-13 21:56:21 +0100

    POJO exercise solved

--- src/main/java/pl/infoshare/structures/_2_lists/_1_warmup/ListExercise1.java
+++ src/main/java/pl/infoshare/structures/_2_lists/_1_warmup/ListExercise1.java
@@ -1,5 +1,11 @@
 package pl.infoshare.structures._2_lists._1_warmup;
 
+import pl.infoshare.structures._1_pojo.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ListExercise1 {
 
     // TODO: Ćwiczenie 1 - API list
@@ -9,6 +15,19 @@ public class ListExercise1 {
     // TODO: 4. Zamień miejscami pierwszy i ostatni element
     // TODO: 5. Wypisz wszystkie pełnoletnie osoby
     public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(
+                new Person("Maciek", 26),
+                new Person("Anastazja", 23),
+                new Person("Mikołaj", 22),
+                new Person("Paweł", 3)
+        );
 
+        System.out.println(persons.get(1));
+        persons.remove(2);
+        for (Person person : persons) {
+            if (person.getAge() >= 18) {
+                System.out.println(person);
+            }
+        }
     }
 }
